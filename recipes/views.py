e413a69718710d0494c298e08d27290148b10fd8
Allan conti junior <allanconti_2012@hotmail.com>
2022-09-16 11:09:14 -0300

    Changes to multiple files

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from re import S
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html')
+
+
+def contato(request):
+    return HttpResponse('recipes/contato')
+
+
+def sobre(request):
+    return HttpResponse('recipes/sobre')
