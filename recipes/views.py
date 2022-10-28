ae1cc750b7b0ca1960cf0d55cde47a088651679f
Allan conti junior <allanconti_2012@hotmail.com>
2022-10-28 12:28:06 -0300

    Removido Contato e Sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,3 @@
-from re import S
-
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Allan Conti Junior',
     })
-
-
-def contato(request):
-    return HttpResponse('recipes/contato')
-
-
-def sobre(request):
-    return HttpResponse('recipes/sobre')
